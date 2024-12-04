53b18846e8dd27ed48b6af0ee0ec140912c7b7d1
Qand_ <86135742+Christophe-Caron@users.noreply.github.com>
2024-12-04 12:15:20 -0500

    Madness.

--- Advent_Of_Code_Day_2/Advent_code_day_2_P.2.py
+++ Advent_Of_Code_Day_2/Advent_code_day_2_P.2.py
@@ -14,66 +14,63 @@ def inc_or_dec(lines):
     run=False
     status='None'
     times=0
-    if times!=len(lines):
-        for line in lines:
-            times+=1
-            fail=0
-            status='None'
-            while True:
+    for line in lines:
+        fail=0
+        status='None'
+        while True:
+            if run:
+                run = False
+                continue
+            if fail>1:
+                fail=0
+                run=False
+                status='None'
+                break
+            for idx, i in enumerate(line[0:len(line)]):
                 if run:
-                    run = False
-                    continue
-                if fail>1:
-                    fail=0
-                    run=False
-                    status='None'
                     break
-                for idx, i in enumerate(line[0:len(line)]):
-                    if run:
-                        break
-                    i=int(i)
-                    for j in line[idx+1:len(line)+1]:
-                            j=int(j)
-                            if i>j:
-                                if status=='Down' or status=='None':
-                                    if abs(i-j)<=3 and abs(i-j)>=1:
-                                        status='Down'
-                                        break
-                                    else:
-                                        del line[idx]
-                                        run=True
-                                        fail+=1
-                                        break
+                i=int(i)
+                for j in line[idx+1:len(line)+1]:
+                        j=int(j)
+                        if i>j:
+                            if status=='Down' or status=='None':
+                                if abs(i-j)<=3 and abs(i-j)>=1:
+                                    status='Down'
+                                    break
                                 else:
+                                    del line[idx]
+                                    run=True
                                     fail+=1
                                     break
-                            elif i<j:
-                                if status=='Up' or status=='None':
-                                    if abs(i-j)<=3 and abs(i-j)>=1:
-                                        status='Up'
-                                        skip=True
-                                        break
-                                    else:
-                                        del line[idx]
-                                        run = True
-                                        fail+=1
-                                        break
+                            else:
+                                fail+=1
+                                break
+                        elif i<j:
+                            if status=='Up' or status=='None':
+                                if abs(i-j)<=3 and abs(i-j)>=1:
+                                    status='Up'
+                                    skip=True
+                                    break
                                 else:
                                     del line[idx]
                                     run = True
                                     fail+=1
                                     break
-                            elif i==j:
+                            else:
                                 del line[idx]
                                 run = True
                                 fail+=1
-                                continue
-                break
-            if status == 'Down' or status == 'Up' and fail<=1:
-                count += 1
-            status='None'
+                                break
+                        elif i==j:
+                            del line[idx]
+                            run = True
+                            fail+=1
+                            continue
             break
-    else:
-        return(count)
+        if status == 'Down' or status == 'Up' and fail<=1:
+            count += 1
+        status='None'
+        continue
+    return(count)
 count=inc_or_dec(line_input())
 print(count)
